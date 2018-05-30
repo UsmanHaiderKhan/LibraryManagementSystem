fc1b38c784b360c642af7f597fabc5d5b3a06b93
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-05-30 20:12:41 +0500

    Update: Dbcontext/Birdge Class Added and set the Entities

---
+++ LibraryManagementSystem/Data/LibraryContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementSystem.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    "Server = DESKTOP-UP0TIG2; Database =LibraryManagementSystem; Trusted_Connection = True; MultipleActiveResultSets = true");
+            }
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
