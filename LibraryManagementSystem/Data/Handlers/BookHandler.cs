505af7556f74564f67fa9fa0ff139fafeca7879b
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-05-30 21:20:59 +0500

    Update:Handler Folder Add Book Handler and Customer handler in Book Handler GetCountBook() and in CustomerHandler GetAllCustomer()

---
+++ LibraryManagementSystem/Data/Handlers/BookHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementSystem.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Data.Handlers
+{
+    public class BookHandler
+    {
+        public Book GetBookById(Customer customer)
+        {
+            var builder = new DbContextOptionsBuilder<LibraryContext>();
+            var db = new LibraryContext(builder.Options);
+            using (db)
+            {
+                return (from c in db.Books where c.BorrowerId == customer.CustomerId select c).FirstOrDefault();
+            }
+        }
+
+        public int BookCount(Customer customer)
+        {
+            var builder = new DbContextOptionsBuilder<LibraryContext>();
+            var db = new LibraryContext(builder.Options);
+            using (db)
+            {
+                return (from c in db.Books where c.BorrowerId == customer.CustomerId select c).Count();
+            }
+        }
+    }
+}
---
+++ LibraryManagementSystem/Data/Handlers/CustomerHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementSystem.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Data.Handlers
+{
+    public class CustomerHandler
+    {
+        public List<Customer> GetAllCustomer()
+        {
+            var builder = new DbContextOptionsBuilder<LibraryContext>();
+
+            using (var db = new LibraryContext(builder.Options))
+            {
+                return (from c in db.Customers select c).ToList();
+            }
+        }
+    }
+}
