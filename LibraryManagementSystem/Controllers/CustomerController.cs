3f6f4b918017cb6187c971e021dc0d01300ba4c8
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-05-30 21:19:12 +0500

    Update:customer controller Get the all customer

---
+++ LibraryManagementSystem/Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using LibraryManagementSystem.Data.Handlers;
+using LibraryManagementSystem.Data.Models;
+using LibraryManagementSystem.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            var customerViewModel = new List<CustomerViewModel>();
+            List<Customer> customers = new CustomerHandler().GetAllCustomer();
+            if (customers.Count == 0)
+            {
+                return View("Empty");
+            }
+
+            foreach (var customer in customers)
+            {
+                customerViewModel.Add(new CustomerViewModel
+                {
+                    Customer = customer,
+                    BookCount = new BookHandler().BookCount(customer)
+                });
+
+            }
+            return View(customerViewModel);
+        }
+    }
+}
