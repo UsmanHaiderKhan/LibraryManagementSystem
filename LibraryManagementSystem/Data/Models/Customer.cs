c192631cb9af5431cbd31e96a62b6fa6f2e630c0
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-05-31 10:39:25 +0500

    Update : the class of customer

--- LibraryManagementSystem/Data/Models/Customer.cs
+++ LibraryManagementSystem/Data/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace LibraryManagementSystem.Data.Models
 {
     public class Customer
     {
+
         public int CustomerId { get; set; }
+
         public string Name { get; set; }
 
     }
