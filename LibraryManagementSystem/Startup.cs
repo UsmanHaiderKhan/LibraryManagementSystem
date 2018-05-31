d64910e2235a3bee0c6bba5909f4a637471c8230
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-05-31 10:37:50 +0500

    Update : necessary configurations in start up class

--- LibraryManagementSystem/Startup.cs
+++ LibraryManagementSystem/Startup.cs
@@ -13,32 +13,26 @@ namespace LibraryManagementSystem
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private IConfigurationRoot _configurationRoot;
+        public Startup(IHostingEnvironment ihostingEnvironment)
         {
-            Configuration = configuration;
+            _configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(ihostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json").Build();
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<LibraryContext>(options => options.UseInMemoryDatabase("LibraryContext"));
+            services.AddDbContext<LibraryContext>(options =>
+                options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection")));
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
 
             app.UseStaticFiles();
 
