2e584dd7a430655616ec7c8bf2f3853df7034688
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-06-02 01:28:56 +0500

    Update Add  view model Lend having two propertise book and customer

---
+++ LibraryManagementSystem/ViewModel/LendViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementSystem.Data.Models;
+
+namespace LibraryManagementSystem.ViewModel
+{
+    public class LendViewModel
+    {
+        public Book Book { get; set; }
+        public IEnumerable<Customer> Customers { get; set; }
+    }
+}
