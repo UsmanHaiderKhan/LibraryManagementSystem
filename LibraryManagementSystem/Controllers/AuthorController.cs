0bfc0452312c7bbde382a2c3a9ff02a1f21a5ae3
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-06-01 18:29:30 +0500

    Update:Adding Author Controller and perform the curd operations and adding Author Handler

---
+++ LibraryManagementSystem/Controllers/AuthorController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using LibraryManagementSystem.Data;
+using LibraryManagementSystem.Data.Handlers;
+using LibraryManagementSystem.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class AuthorController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            List<Author> authors = new AuthorHandler().GetAlAuthorsWithBooks();
+            if (authors.Count == 0)
+            {
+                return View("Empty");
+            }
+            return View(authors);
+        }
+        [HttpGet]
+        public IActionResult UpdateAuthor(int id)
+        {
+            var author = new AuthorHandler().GetAuthorById(id);
+            return View(author);
+        }
+        [HttpPost]
+        public IActionResult UpdateAuthor(Author author)
+        {
+            var builder = new DbContextOptionsBuilder<LibraryContext>();
+            var db = new LibraryContext(builder.Options);
+            if (author != null)
+            {
+                db.Entry(author).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            else
+            {
+                return NotFound();
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult AddAuthor()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddAuthore(Author author)
+        {
+            var builder = new DbContextOptionsBuilder<LibraryContext>();
+            var db = new LibraryContext(builder.Options);
+            using (db)
+            {
+                if (author != null)
+                {
+                    if (ModelState.IsValid)
+                    {
+                        db.Authors.Add(author);
+                        db.SaveChanges();
+                    }
+
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteAuthor(int id)
+        {
+            var builder = new DbContextOptionsBuilder<LibraryContext>();
+            var db = new LibraryContext(builder.Options);
+            var author = new AuthorHandler().GetAuthorById(id);
+            using (db)
+            {
+                if (author != null)
+                {
+                    db.Entry(author).State = EntityState.Deleted;
+                    db.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ LibraryManagementSystem/Data/Handlers/AuthorHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementSystem.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Data.Handlers
+{
+    public class AuthorHandler
+    {
+        public List<Author> GetAlAuthorsWithBooks()
+        {
+            var builder = new DbContextOptionsBuilder<LibraryContext>();
+            var db = new LibraryContext(builder.Options);
+            using (db)
+            {
+                return (from c in db.Authors.Include(m => m.Books) select c).ToList();
+            }
+        }
+
+        public Author GetAuthorById(int id)
+        {
+            var builder = new DbContextOptionsBuilder<LibraryContext>();
+            var db = new LibraryContext(builder.Options);
+            using (db)
+            {
+                return (from c in db.Authors where c.AuthorId == id select c).FirstOrDefault();
+            }
+        }
+
+    }
+}
