0de8c6fe97074c2db4029a72217940bed9d08ff7
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-06-02 00:27:02 +0500

    Update : Add a BookViewModel  and Added Two Properties

---
+++ LibraryManagementSystem/ViewModel/BookViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementSystem.Data.Models;
+
+namespace LibraryManagementSystem.ViewModel
+{
+    public class BookViewModel
+    {
+        public Book Book { get; set; }
+        public IEnumerable<Author> Authors { get; set; }
+
+    }
+}
