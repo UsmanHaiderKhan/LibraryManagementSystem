a75667c3e41352ec7057c883e35bc77fec7b6fb9
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-06-02 01:28:00 +0500

    Update :Add a LendController or form the methods index and Lend book

---
+++ LibraryManagementSystem/Controllers/LendController.cs
@@ -0,0 +1,53 @@
+﻿using LibraryManagementSystem.Data;
+using LibraryManagementSystem.Data.Handlers;
+using LibraryManagementSystem.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class LendController : Controller
+    {
+        // GET
+        public IActionResult Index(int num)
+        {
+            var availableBooks = new BookHandler().GetBookwithAuthorBorrower(num);
+            if (availableBooks.Count == 0)
+            {
+                return View("Empty");
+            }
+            else
+            {
+                return View(availableBooks);
+            }
+
+        }
+        [HttpGet]
+        public IActionResult LendBook(int bookId)
+        {
+            var lendViewModel = new LendViewModel()
+            {
+                Book = new BookHandler().GetBookById(bookId),
+                Customers = new CustomerHandler().GetAllCustomer()
+            };
+            return View(lendViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult LendBook(LendViewModel lendViewModel)
+        {
+            var builder = new DbContextOptionsBuilder<LibraryContext>();
+            var db = new LibraryContext(builder.Options);
+            var book = new BookHandler().GetBookById(lendViewModel.Book.BookId);
+            var customer = new CustomerHandler().GetCustomerById(lendViewModel.Book.BorrowerId);
+            book.Borrower = customer;
+            using (db)
+            {
+                db.Entry(book).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
