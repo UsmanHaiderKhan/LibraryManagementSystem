abe1d80fef79103ed86356e30787db7139afb5b3
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-06-05 09:44:46 +0500

    Added New Home Controller for Front End Or Showing the  Start Page

---
+++ LibraryManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using LibraryManagementSystem.Data.Handlers;
+using LibraryManagementSystem.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel()
+            {
+                AuthorCount = new AuthorHandler().GetAuthorCount(),
+                CustomerCount = new CustomerHandler().GetCustomerCount(),
+                BookCount = new BookHandler().GetBookCount(),
+                LendBookCount = new BookHandler().GetLendCount(x => x.Customer != null)
+            };
+            return View(homeViewModel);
+        }
+    }
+}
