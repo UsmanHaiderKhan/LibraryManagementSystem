f202fe31d66af0a33a42a21fb2e0f26e8e087c4f
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-06-05 09:48:32 +0500

    Added Two New View Model HomeView Model And AuthorViewModel

---
+++ LibraryManagementSystem/ViewModel/AuthorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementSystem.Data.Models;
+
+namespace LibraryManagementSystem.ViewModel
+{
+    public class AuthorViewModel
+    {
+        public Author Author { get; set; }
+        public string Referer { get; set; }
+
+    }
+}
---
+++ LibraryManagementSystem/ViewModel/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.ViewModel
+{
+    public class HomeViewModel
+    {
+        public int BookCount { get; set; }
+        public int CustomerCount { get; set; }
+        public int LendBookCount { get; set; }
+        public int AuthorCount { get; set; }
+
+    }
+}
