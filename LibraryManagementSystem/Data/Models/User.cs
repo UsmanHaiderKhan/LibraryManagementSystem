b8707e4967105af16423513a52e415ceff4d8f6b
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-07-10 17:35:44 +0500

    Update:Added Validation on the User Class

--- LibraryManagementSystem/Data/Models/User.cs
+++ LibraryManagementSystem/Data/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,22 @@ namespace LibraryManagementSystem.Data.Models
     public class User
     {
         public int Id { get; set; }
+        [Required]
         public string LoginId { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
+        [Required]
         public string FullAddress { get; set; }
+        [Required]
         public long PhoneNo { get; set; }
+        [Required]
         public string SecurityQuestion { get; set; }
         public string SecurityAnswer { get; set; }
     }
