6c982f3470da4b4c96b1b5d9514870ed9ca9765f
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-07-10 17:29:51 +0500

    Add New USer Class in the Model Folder

---
+++ LibraryManagementSystem/Data/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Data.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string LoginId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string FullAddress { get; set; }
+        public long PhoneNo { get; set; }
+        public string SecurityQuestion { get; set; }
+        public string SecurityAnswer { get; set; }
+    }
+}
