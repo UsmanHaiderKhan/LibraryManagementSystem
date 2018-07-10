57bb2680706ea1d270986ec825bc66e87806ca1a
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-07-10 17:36:12 +0500

    UPdate Add the Login view Model to Get the Logid or password

---
+++ LibraryManagementSystem/ViewModel/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.ViewModel
+{
+    public class LoginViewModel
+    {
+
+        public string LoginId { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
