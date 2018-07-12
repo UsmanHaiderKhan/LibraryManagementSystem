dbd0d9ad990e377ec2c2381f828108e3fbd45eca
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-07-12 09:47:19 +0500

    Add New Contact  Controller with index action

---
+++ LibraryManagementSystem/Controllers/ContactController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return
+            View();
+        }
+    }
+}
