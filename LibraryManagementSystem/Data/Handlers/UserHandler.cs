d138db4b02d679c7b2d7d99af14ba4a46a062662
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-07-15 11:05:04 +0500

    Add New Class Name it usershandler add a method getuser

---
+++ LibraryManagementSystem/Data/Handlers/UserHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementSystem.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Data.Handlers
+{
+    public class UserHandler
+    {
+        public User GetUser(string Loginid, string Password)
+        {
+            var builder = new DbContextOptionsBuilder<LibraryContext>();
+            var db = new LibraryContext(builder.Options);
+            using (db)
+            {
+                return (from v in db.Users where v.LoginId.Equals(Loginid) && v.Password.Equals(Password) select v).FirstOrDefault();
+            }
+        }
+    }
+}
