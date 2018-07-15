88eb974ed5b3cad843fae9c9bcc84aff062e96e2
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-07-15 11:04:23 +0500

    Update User Controller butt Problem in class or key word name spaces

--- LibraryManagementSystem/Controllers/UserController.cs
+++ LibraryManagementSystem/Controllers/UserController.cs
@@ -1,5 +1,8 @@
-﻿using LibraryManagementSystem.Data.Models;
+﻿using System;
+using LibraryManagementSystem.Data.Handlers;
+using LibraryManagementSystem.Data.Models;
 using LibraryManagementSystem.ViewModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryManagementSystem.Controllers
@@ -14,6 +17,18 @@ namespace LibraryManagementSystem.Controllers
         [HttpGet]
         public IActionResult Login()
         {
+            //HttpCookie myCookie = Request.Cookies["logsec"];
+            //if (myCookie != null)
+            //{
+            //    User user = new UserHandler().GetUser(myCookie.Values["logid"], myCookie.Values["psd"]);
+            //    if (user != null)
+            //    {
+            //        myCookie.Expires = DateTime.Now.AddDays(7);
+            //       ISession(Webutils.Current_User, user);
+            //    }
+            //}
+            //ViewBag.Controller = Request.QueryString["ctl"];
+            //ViewBag.Action = Request.QueryString["act"];
             return View();
         }
         [HttpPost]
