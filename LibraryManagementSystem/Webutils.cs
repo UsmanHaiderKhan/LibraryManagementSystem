ec8fc8015eba932dbb7386e7ea25ad1eb54138dc
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-07-15 11:06:18 +0500

    Add a New Class with name of webutils which contain the name of admin or session of login

---
+++ LibraryManagementSystem/Webutils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    public class Webutils
+    {
+        public const string Current_User = "Current User Name";
+        public const int Admin = 1;
+    }
+}
